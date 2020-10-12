03eb3f92e03fde89c2f08aa45a7e6aacb0392059
Marphenn <70971709+Marphenn@users.noreply.github.com>
2020-10-12 18:34:55 +0300

    Update Football.cpp

--- Football.cpp
+++ Football.cpp
@@ -6,60 +6,69 @@ using namespace std;
 
 string getName()
 {
-	string n;
-	bool p = 1;
-	while (p)
-	{
-		cout << "\nEnter player's name: " << endl;
-		getline(cin, n);
-		p = 0;
-		for (int i = 0; n[i] != 0; i++)
-		{
-			if (n[i] >= '0' && n[i] <= '9')
-			{
-				cout << "Error.";
-				p = 1;
-				break;
-			}
-		}
-		rewind(stdin);
-	}
-	cout << "Your name: " << n << endl;
-	return n;
+    string n;
+    bool p = 1;
+    while (p)
+    {
+        cout << "\nEnter player's name: " << endl;
+        getline(cin, n);
+        p = 0;
+        for (int i = 0; n[i] != 0; i++)
+        {
+            if (n[i] >= '0' && n[i] <= '9')
+            {
+                cout << "Error.";
+                p = 1;
+                break;
+            }
+        }
+        rewind(stdin);
+    }
+    cout << "Your name: " << n << endl;
+    return n;
 }
 
 int getAge()
 {
-	int age;
-	while (1)
-	{
-		cout << "\nEnter player's age: " << endl;
-		cin >> age;
-		if (cin.fail() || age > 50 || age < 15)
-		{
-			cout << "\nIncorrect value. Please, try again." << endl;
-			cin.clear();
-			cin.ignore(32767, '\n');
-		}
-		else return age;
-	}
+    int age;
+    while (1)
+    {
+        cout << "\nEnter player's age: " << endl;
+        cin >> age;
+        if (cin.fail() || age > 50 || age < 15)
+        {
+            cout << "\nIncorrect value. Please, try again." << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+        }
+        else
+        {
+            cin.ignore(32767, '\n');
+            return age;
+        }
+    }
 }
 
 double getSalary()
 {
-	double salary;
-	while (1)
-	{
-		cout << "\nEnter player's salary: " << endl;
-		cin >> salary;
-		if (cin.fail() || salary < 0)
-		{
-			cout << "Incorrect value. Please, try again." << endl;
-			cin.clear();
-			cin.ignore(32767, '\n');
-		}
-		else return salary;
-	}
+    double salary;
+    while (1)
+    {
+        cout << "\nEnter player's salary: " << endl;
+        cin >> salary;
+        if (cin.fail() || salary < 0)
+        {
+            cout << "Incorrect value. Please, try again." << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+        }
+        else
+        {
+            cin.ignore(32767, '\n');
+            return salary;
+        }
+    }
+    
 }
 
 void Football::getRules()
@@ -67,121 +76,121 @@ void Football::getRules()
 	cout << endl << "RULES:\n--- Match duration (min): " << m_duration << "\n--- Number of players in one team: " << n_players << "\n--- Possible substitutions: " << substitutions << endl;
 }
 
-void Football::f_rules_menu()
+void f_rules_menu()
 {
-	cout << "\nWhat part of rules do you want to change?\n1 --- Match duration\n2 --- Number of players in one team\n3 --- Number of possible substitutions\n-1 --- Exit\n";
+    cout << "\nWhat part of rules do you want to change?\n1 --- Match duration\n2 --- Number of players in one team\n3 --- Number of possible substitutions\n-1 --- Exit\n";
 }
 
-int Football::choice(int n_variants = 3)
+int choice(int n_variants = 3)
 {
-	int choice;
-	while (1)
-	{
-		cin >> choice;
-		if (cin.fail() || (choice > 3 || choice < 1) && choice != -1)
-		{
-			cout << "Incorrect value." << endl;
-			cin.clear();
-			cin.ignore(32767, '\n');
-		}
-		else return choice;
-	}
+    int choice;
+    while (1)
+    {
+        cin >> choice;
+        if (cin.fail() || (choice > 3 || choice < 1) && choice != -1)
+        {
+            cout << "Incorrect value." << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+        }
+        else return choice;
+    }
 }
 
-void Football::set_m_duration()
+int set_m_duration()
 {
-	int duration;
-	while (1)
-	{
-		cout << "Enter new match duration (minutes) : " << endl;
-		cin >> duration;
-		if (cin.fail() || duration < 30)
-		{
-			cout << "Incorrect value." << endl;
-			cin.clear();
-			cin.ignore(32767, '\n');
-		}
-		else break;
-	}
-	m_duration = duration;
+    int duration;
+    while (1)
+    {
+        cout << "Enter new match duration (minutes) : " << endl;
+        cin >> duration;
+        if (cin.fail() || duration < 30)
+        {
+            cout << "Incorrect value." << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+        }
+        else break;
+    }
+   return duration;
 }
 
-void Football::set_n_players()
+int set_n_players()
 {
-	int n;
-	while (1)
-	{
-		cout << "Enter new number of players in one team: " << endl;
-		cin >> n;
-		if (cin.fail() || n > 11 || n < 2)
-		{
-			cout << "Incorrect value." << endl;
-			cin.clear();
-			cin.ignore(32767, '\n');
-		}
-		else break;
-	}
-	n_players = n;
+    int n;
+    while (1)
+    {
+        cout << "Enter new number of players in one team: " << endl;
+        cin >> n;
+        if (cin.fail() || n > 11 || n < 2)
+        {
+            cout << "Incorrect value." << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+        }
+        else break;
+    }
+    return n;
 }
 
-void Football::set_max_substitutions()
+int set_max_substitutions(int players)
 {
-	int sub;
-	while (1)
-	{
-		cout << "Enter new number of possible substitutions: " << endl;
-		cin >> sub;
-		if (cin.fail() || sub <0 || sub > n_players - 1)
-		{
-			cout << "Incorrect value." << endl;
-			cin.clear();
-			cin.ignore(32767, '\n');
-		}
-		else break;
-	}
-	substitutions = sub;
+    int sub;
+    while (1)
+    {
+        cout << "Enter new number of possible substitutions: " << endl;
+        cin >> sub;
+        if (cin.fail() || sub <0 || sub > players-1)
+        {
+            cout << "Incorrect value." << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+        }
+        else break;
+    }
+   return sub;
 }
 
 void Football::setRules()
 {
-	while (1)
-	{
-		f_rules_menu();
-		int c = choice();
-		switch (c)
-		{
-		case 1: set_m_duration(); break;
-		case 2: set_n_players(); break;
-		case 3: set_max_substitutions(); break;
-		default: break;
-		}
-		if (c == -1) break;
-		system("CLS");
-	}
+    while (1)
+    {
+        f_rules_menu();
+        int c = choice();
+        switch (c)
+        {
+        case 1: m_duration = set_m_duration(); break;
+        case 2: n_players = set_n_players(); break;
+        case 3: substitutions = set_max_substitutions(n_players); break;
+        default: break;
+        }
+        if (c == -1) break;
+        system("CLS");
+    }
 }
 
-void BPL::getInfo()
+void BPL::BPLInfo()
 {
-	cout << endl << "BPL INFO:\n--- Country: " << country << "\n--- Number of teams: " << n_teams << "\n--- Number of tours: " << n_tours << endl;
+    cout << endl << "BPL INFO:\n--- Country: " << country << "\n--- Number of teams: " << n_teams << "\n--- Number of tours: " << n_tours << endl;
 }
 
-void Bundesliga::getInfo()
+void Bundesliga::BundesInfo()
 {
-	cout << endl << "BUNDESLIGA INFO:\n--- Country: " << country << "\n--- Number of teams: " << n_teams << "\n--- Number of tours: " << n_tours << endl;
+    cout << endl << "BUNDESLIGA INFO:\n--- Country: " << country << "\n--- Number of teams: " << n_teams << "\n--- Number of tours: " << n_tours << endl;
 }
 
-void ManCity::getInfo()
+void ManCity::ManCityInfo()
 {
-	cout << endl << "MANCHESTER CITY INFO:\n--- Coach: " << coach << "\n--- Current place in the table: " << position << endl;
+    cout << endl << "MANCHESTER CITY INFO:\n--- Coach: " << coach << "\n--- Current place in the table: " << position << endl;
 }
 
-void BorussiaD::getInfo()
+void BorussiaD::BorDInfo()
 {
-	cout << endl << "BORUSSIA DORTMUND INFO:\n--- Coach: " << coach << "\n--- Current place in the table: " << position << endl;
+    cout << endl << "BORUSSIA DORTMUND INFO:\n--- Coach: " << coach << "\n--- Current place in the table: " << position << endl;
 }
 
 void TransferPlayer::getPlayerInfo()
 {
-	cout << endl << "TRANSFER PLAYER INFO:\n--- Name: " << p_name << "\n--- Age: " << age << "\n--- Salary: " << salary << endl;
+    cout << endl << "TRANSFER PLAYER INFO:\n--- Name: " << p_name << "\n--- Age: " << age << "\n--- Salary: " << salary << endl;
 }
 
