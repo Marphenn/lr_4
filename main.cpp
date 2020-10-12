eef82e2ff91c8c7fcc038492800a6a3552f3cba1
Marphenn <70971709+Marphenn@users.noreply.github.com>
2020-10-12 18:22:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,8 +13,15 @@ int main()
 	string name = getName();
 	int age = getAge();
 	double salary = getSalary();
-	TransferPlayer *p1=new TransferPlayer(name,age,salary);
+	TransferPlayer *p1=new TransferPlayer(name, age, salary);
+	p1->setRules(); 
+	p1->getRules();
+	p1->BPLInfo();
+	p1->ManCityInfo();
+	p1->BundesInfo();
+	p1->BorDInfo();
 	p1->getPlayerInfo();
+	
 	delete p1;
 
 	return 0;
