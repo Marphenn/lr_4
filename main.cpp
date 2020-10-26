e1b440528b090ff0fdb1feb111f5d0b7d0433776
Marphenn <70971709+Marphenn@users.noreply.github.com>
2020-10-26 14:51:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
-#include <string>
 #include "Football.h"
 
 using namespace std;
+
+int set_m_duration();
+int set_n_players();
+int set_max_substitutions();
+
+int set_num_teams(const char*);
+int set_num_tours(const char*);
+
+int set_position(const char*, int);
 
 string getName();
 int getAge();
@@ -10,19 +18,21 @@ double getSalary();
 
 int main()
 {
+	int dur = set_m_duration();
+	int pl = set_n_players();
+	int subs = set_max_substitutions();
+	int BPLteams = set_num_teams("BPL");
+	int BPLtours = set_num_tours("BPL");
+	int BundesTeams = set_num_teams("BUNDESLIGA");
+	int BundesTours = set_num_tours("BUNDESLIGA");
+	int MCpos = set_position("Manchester City", BPLteams);
+	int BorDpos = set_position("Borussia Dortmund", BundesTeams);
 	string name = getName();
 	int age = getAge();
 	double salary = getSalary();
-	TransferPlayer *p1=new TransferPlayer(name, age, salary);
-	p1->setRules(); 
-	p1->getRules();
-	p1->BPLInfo();
-	p1->ManCityInfo();
-	p1->BundesInfo();
-	p1->BorDInfo();
-	p1->getPlayerInfo();
-	
-	delete p1;
+
+	TransferPlayer p1(dur, pl, subs, BPLteams, BPLtours, MCpos, BundesTeams, BundesTours, BorDpos, name, age, salary);
+	p1.getPlayerInfo();
 
 	return 0;
 }
