e909a05962982e2baac9cd552fef7619d9b39269
Marphenn <70971709+Marphenn@users.noreply.github.com>
2020-10-26 14:50:12 +0300

    Update Football.cpp

--- Football.cpp
+++ Football.cpp
@@ -4,6 +4,144 @@
 
 using namespace std;
 
+int set_m_duration()
+{
+    int duration;
+    while (1)
+    {
+        cout << "Enter match duration (minutes) : " << endl;
+        cin >> duration;
+        if (cin.fail() || duration < 30)
+        {
+            cout << "Incorrect value." << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+        }
+        else return duration;
+    }
+}
+
+int set_n_players()
+{
+    int n;
+    while (1)
+    {
+        cout << "Enter the number of players in one team: " << endl;
+        cin >> n;
+        if (cin.fail() || n > 11 || n < 2)
+        {
+            cout << "Incorrect value." << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+        }
+        else return n;
+    }
+}
+
+int set_max_substitutions()
+{
+    int sub;
+    while (1)
+    {
+        cout << "Enter the number of possible substitutions: " << endl;
+        cin >> sub;
+        if (cin.fail() || sub < 0 || sub > 5)
+        {
+            cout << "Incorrect value." << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+        }
+        else return sub;
+    }
+}
+
+int set_num_teams(const char* champ)
+{
+    int teams;
+    while (1)
+    {
+        cout << "Enter the number of teams in "<< champ << ": " << endl;
+        cin >> teams;
+        if (cin.fail() || teams < 0 || teams > 40)
+        {
+            cout << "Incorrect value." << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+        }
+        else return teams;
+    }
+}
+
+int set_num_tours(const char* champ)
+{
+    int tours;
+    while (1)
+    {
+        cout << "Enter the number of tours in " << champ << ": " << endl;
+        cin >> tours;
+        if (cin.fail() || tours < 0 || tours > 50)
+        {
+            cout << "Incorrect value." << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+        }
+        else return tours;
+    }
+}
+
+int set_position(const char* team, int max)
+{
+    int pos;
+    while (1)
+    {
+        cout << "Enter the position of " << team << ": " << endl;
+        cin >> pos;
+        if (cin.fail() || pos < 0 || pos > max)
+        {
+            cout << "Incorrect value." << endl;
+            cin.clear();
+            cin.ignore(32767, '\n');
+        }
+        else return pos;
+    }
+}
+
+void Football::getRules()
+{
+	cout << endl << "RULES:\n--- Match duration (min): " << m_duration << "\n--- Number of players in one team: " << n_players << "\n--- Possible substitutions: " << substitutions << endl;
+}
+
+void BPL::getBPLInfo()
+{
+    cout << endl << "BPL INFO:" << "\n--- Number of teams: " << n_teams << "\n--- Number of tours: " << n_tours << endl;
+}
+
+void Bundesliga::getBundesInfo()
+{
+    cout << endl << "BUNDESLIGA INFO:" << "\n--- Number of teams: " << n_teams << "\n--- Number of tours: " << n_tours << endl;
+}
+
+void ManCity::getManCityInfo()
+{
+    cout << endl << "MANCHESTER CITY INFO:" << "\n--- Current place in the table: " << position << endl;
+}
+
+void BorussiaD::getBorDInfo()
+{
+    cout << endl << "BORUSSIA DORTMUND INFO:" << "\n--- Current place in the table: " << position << endl;
+}
+
+void TransferPlayer::getPlayerInfo()
+{
+    system("CLS");
+    cout << endl << "TRANSFER PLAYER INFO:\n--- Name: " << p_name << "\n--- Age: " << age << "\n--- Salary: " << salary << endl;
+    getManCityInfo();
+    getBPLInfo();
+    getBorDInfo();
+    getBundesInfo();
+    getRules();
+}
+
 string getName()
 {
     string n;
@@ -11,6 +149,7 @@ string getName()
     while (p)
     {
         cout << "\nEnter player's name: " << endl;
+        rewind(stdin);
         getline(cin, n);
         p = 0;
         for (int i = 0; n[i] != 0; i++)
@@ -22,9 +161,7 @@ string getName()
                 break;
             }
         }
-        rewind(stdin);
     }
-    cout << "Your name: " << n << endl;
     return n;
 }
 
@@ -41,11 +178,7 @@ int getAge()
             cin.clear();
             cin.ignore(32767, '\n');
         }
-        else
-        {
-            cin.ignore(32767, '\n');
-            return age;
-        }
+        else return age;
     }
 }
 
@@ -62,135 +195,6 @@ double getSalary()
             cin.clear();
             cin.ignore(32767, '\n');
         }
-        else
-        {
-            cin.ignore(32767, '\n');
-            return salary;
-        }
-    }
-    
-}
-
-void Football::getRules()
-{
-	cout << endl << "RULES:\n--- Match duration (min): " << m_duration << "\n--- Number of players in one team: " << n_players << "\n--- Possible substitutions: " << substitutions << endl;
-}
-
-void f_rules_menu()
-{
-    cout << "\nWhat part of rules do you want to change?\n1 --- Match duration\n2 --- Number of players in one team\n3 --- Number of possible substitutions\n-1 --- Exit\n";
-}
-
-int choice(int n_variants = 3)
-{
-    int choice;
-    while (1)
-    {
-        cin >> choice;
-        if (cin.fail() || (choice > 3 || choice < 1) && choice != -1)
-        {
-            cout << "Incorrect value." << endl;
-            cin.clear();
-            cin.ignore(32767, '\n');
-        }
-        else return choice;
+        else return salary;
     }
 }
-
-int set_m_duration()
-{
-    int duration;
-    while (1)
-    {
-        cout << "Enter new match duration (minutes) : " << endl;
-        cin >> duration;
-        if (cin.fail() || duration < 30)
-        {
-            cout << "Incorrect value." << endl;
-            cin.clear();
-            cin.ignore(32767, '\n');
-        }
-        else break;
-    }
-   return duration;
-}
-
-int set_n_players()
-{
-    int n;
-    while (1)
-    {
-        cout << "Enter new number of players in one team: " << endl;
-        cin >> n;
-        if (cin.fail() || n > 11 || n < 2)
-        {
-            cout << "Incorrect value." << endl;
-            cin.clear();
-            cin.ignore(32767, '\n');
-        }
-        else break;
-    }
-    return n;
-}
-
-int set_max_substitutions(int players)
-{
-    int sub;
-    while (1)
-    {
-        cout << "Enter new number of possible substitutions: " << endl;
-        cin >> sub;
-        if (cin.fail() || sub <0 || sub > players-1)
-        {
-            cout << "Incorrect value." << endl;
-            cin.clear();
-            cin.ignore(32767, '\n');
-        }
-        else break;
-    }
-   return sub;
-}
-
-void Football::setRules()
-{
-    while (1)
-    {
-        f_rules_menu();
-        int c = choice();
-        switch (c)
-        {
-        case 1: m_duration = set_m_duration(); break;
-        case 2: n_players = set_n_players(); break;
-        case 3: substitutions = set_max_substitutions(n_players); break;
-        default: break;
-        }
-        if (c == -1) break;
-        system("CLS");
-    }
-}
-
-void BPL::BPLInfo()
-{
-    cout << endl << "BPL INFO:\n--- Country: " << country << "\n--- Number of teams: " << n_teams << "\n--- Number of tours: " << n_tours << endl;
-}
-
-void Bundesliga::BundesInfo()
-{
-    cout << endl << "BUNDESLIGA INFO:\n--- Country: " << country << "\n--- Number of teams: " << n_teams << "\n--- Number of tours: " << n_tours << endl;
-}
-
-void ManCity::ManCityInfo()
-{
-    cout << endl << "MANCHESTER CITY INFO:\n--- Coach: " << coach << "\n--- Current place in the table: " << position << endl;
-}
-
-void BorussiaD::BorDInfo()
-{
-    cout << endl << "BORUSSIA DORTMUND INFO:\n--- Coach: " << coach << "\n--- Current place in the table: " << position << endl;
-}
-
-void TransferPlayer::getPlayerInfo()
-{
-    cout << endl << "TRANSFER PLAYER INFO:\n--- Name: " << p_name << "\n--- Age: " << age << "\n--- Salary: " << salary << endl;
-}
-
