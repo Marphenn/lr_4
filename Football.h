63e2824abddb73d4abd19e1baee50b02ff2c1827
Marphenn <70971709+Marphenn@users.noreply.github.com>
2020-10-26 14:50:33 +0300

    Update Football.h

--- Football.h
+++ Football.h
@@ -8,93 +8,81 @@ private:
 	int m_duration;
 	int n_players;
 	int substitutions;
-	
+
 public:
-	Football()
-		: m_duration(90), n_players(11), substitutions(3)
+	Football(int dur = 90, int players = 11, int subs = 3)
+		: m_duration(dur), n_players(players), substitutions(subs)
 	{ };
 	~Football() {
 		cout << "\nFootball destroyed.\n";
 	};
 	void getRules();
-	void setRules();
 };
 
 
 class BPL : virtual public Football
 {
 private:
-	const char* country;
 	int n_teams;
 	int n_tours;
 public:
-	BPL(int teams=20, int tours=38)
-		: country("England"), n_teams(teams), n_tours(tours)
-	{
-		//setRules();
-	};
+	BPL(int dur = 90, int players = 11, int subs = 3, int teams = 20, int tours = 38)
+		: Football(dur, players, subs), n_teams(teams), n_tours(tours)
+	{};
 	~BPL()
 	{
 		cout << "\nBPL destroyed.\n";
 	}
-	void BPLInfo();
+	void getBPLInfo();
 };
 
 
 class Bundesliga : virtual public Football
 {
 private:
-	const char* country;
 	int n_teams;
 	int n_tours;
 public:
 
-	Bundesliga(int teams=18, int tours=34)
-		: country("Germany"), n_teams(teams), n_tours(tours)
-	{
-		//setRules();
-	};
+	Bundesliga(int dur = 90, int players = 11, int subs = 3, int teams = 18, int tours = 34)
+		: Football(dur, players, subs), n_teams(teams), n_tours(tours)
+	{};
 	~Bundesliga()
 	{
 		cout << "\nBundesliga destroyed.\n";
 	}
-	void BundesInfo();
+	void getBundesInfo();
 };
-
 
 class ManCity : public BPL
 {
 private:
-	string coach;
 	int position;
 public:
-	ManCity(string c = "Pep Guardiola", int p = 14)
-		: coach(c), position(p)
+	ManCity(int dur = 90, int players = 11, int subs = 3, int teams = 20, int tours = 38, int p = 14)
+		: BPL(dur, players, subs, teams, tours), position(p)
 	{};
 	~ManCity()
 	{
 		cout << "\nManchester City destroyed.\n";
 	}
-	void ManCityInfo();
+	void getManCityInfo();
 };
-
 
 class BorussiaD : public Bundesliga
 {
 private:
-	string coach;
 	int position;
 public:
-	BorussiaD(string c = "Lucien Favre", int p = 5)
-		: coach(c), position(p)
+	BorussiaD(int dur = 90, int players = 11, int subs = 3, int teams = 18, int tours = 34, int p = 5)
+		: Bundesliga(dur, players, subs, teams, tours), position(p)
 	{};
 	~BorussiaD()
 	{
 		cout << "\nBorussia Dortmund destroyed.\n";
 	}
-	void BorDInfo();
+	void getBorDInfo();
 };
-
 
 class TransferPlayer : public ManCity, public BorussiaD
 {
@@ -103,8 +91,8 @@ private:
 	int age;
 	double salary;
 public:
-	TransferPlayer(string n = "Sancho", int a = 20, double s = 38.500)
-		: p_name(n), age(a), salary(s)
+	TransferPlayer(int dur = 90, int players = 11, int subs = 3, int teams1 = 20, int tours1 = 38, int p1 = 14, int teams2 = 18, int tours2 = 34, int p2 = 5, string n = "Sancho", int a = 20, double s = 38.500)
+		: Football(dur, players, subs), ManCity(dur, players, subs, teams1, tours1, p1), BorussiaD(dur, players, subs, teams2, tours2, p2), p_name(n), age(a), salary(s)
 	{};
 	~TransferPlayer()
 	{
